9049d51b4952de5bf8e4c5b21692b0bf47d000f1
Christine Clark <chinita96@gmail.com>
2018-05-31 17:38:33 -0400

    Add more tests.

--- src/components/credits/ManageCreditPage.spec.js
+++ src/components/credits/ManageCreditPage.spec.js
@@ -3,6 +3,44 @@ import {mount} from 'enzyme';
 import {ManageCreditPage} from './ManageCreditPage';
 
 describe('Manage Credit Page', () => {
+  it('creates a new credit successfully', () => {
+    const props = {
+      actions: { saveCredit: () => Promise.resolve() },
+      categories: [],
+      credit: {id: '', postDate: '01/01/2018', description: 'Freelance', amount: '100.00', category: 'miscellaneous'}
+    };
+    const wrapper = mount(<ManageCreditPage {...props} />);
+    const h1Text = wrapper.find('h1').text();
+    const saveButton = wrapper.find('input').last();
+
+    saveButton.simulate('click');
+    expect(h1Text).toBe('Add New Credit');
+    expect(wrapper.state().credit.postDate).toBe('01/01/2018');
+    expect(wrapper.state().credit.amount).toBe('100.00');
+    expect(wrapper.state().errors).toEqual({});
+  });
+
+  it('updates an existing credit successfully', () => {
+    const props = {
+      actions: { saveCredit: () => Promise.resolve() },
+      categories: [],
+      credit: {id: '1', postDate: '01/01/2018', description: 'Freelance', amount: '100.00', category: 'miscellaneous'}
+    };
+    const wrapper = mount(<ManageCreditPage {...props} />);
+    const h1Text = wrapper.find('h1').text();
+    const amountInput = wrapper.find('input[name="amount"]').first();
+    const saveButton = wrapper.find('input').last();
+    expect(wrapper.state().credit.amount).toBe('100.00');
+
+    amountInput.simulate('change', {target: {name: 'amount', value: '150.00'}});
+    saveButton.simulate('click');
+
+    expect(h1Text).toBe('Update Credit');
+    expect(wrapper.state().credit.postDate).toBe('01/01/2018');
+    expect(wrapper.state().credit.amount).toBe('150.00');
+    expect(wrapper.state().errors).toEqual({});
+  });
+
   it('sets error message when trying to save empty post date', () => {
     const props = {
       actions: { saveCredit: () => Promise.resolve() },
--- src/components/debits/ManageDebitPage.spec.js
+++ src/components/debits/ManageDebitPage.spec.js
@@ -3,6 +3,44 @@ import {mount} from 'enzyme';
 import {ManageDebitPage} from './ManageDebitPage';
 
 describe('Manage Debit Page', () => {
+  it('creates a new debit successfully', () => {
+    const props = {
+      actions: { saveDebit: () => Promise.resolve() },
+      categories: [],
+      debit: {id: '', postDate: '01/01/2018', description: 'Publix', amount: '100.00', category: 'groceries'}
+    };
+    const wrapper = mount(<ManageDebitPage {...props} />);
+    const h1Text = wrapper.find('h1').text();
+    const saveButton = wrapper.find('input').last();
+
+    saveButton.simulate('click');
+    expect(h1Text).toBe('Add New Debit');
+    expect(wrapper.state().debit.postDate).toBe('01/01/2018');
+    expect(wrapper.state().debit.amount).toBe('100.00');
+    expect(wrapper.state().errors).toEqual({});
+  });
+
+  it('updates an existing debit successfully', () => {
+    const props = {
+      actions: { saveDebit: () => Promise.resolve() },
+      categories: [],
+      debit: {id: '1', postDate: '01/01/2018', description: 'Publix', amount: '100.00', category: 'groceries'}
+    };
+    const wrapper = mount(<ManageDebitPage {...props} />);
+    const h1Text = wrapper.find('h1').text();
+    const amountInput = wrapper.find('input[name="amount"]').first();
+    const saveButton = wrapper.find('input').last();
+    expect(wrapper.state().debit.amount).toBe('100.00');
+
+    amountInput.simulate('change', {target: {name: 'amount', value: '150.00'}});
+    saveButton.simulate('click');
+
+    expect(h1Text).toBe('Update Debit');
+    expect(wrapper.state().debit.postDate).toBe('01/01/2018');
+    expect(wrapper.state().debit.amount).toBe('150.00');
+    expect(wrapper.state().errors).toEqual({});
+  });
+
   it('sets error message when trying to save empty post date', () => {
     const props = {
       actions: { saveDebit: () => Promise.resolve() },
